5ef392444cc2989e12a4e7bb592e36c5afc09511
Dominick Baier <dbaier@leastprivilege.com>
2014-09-13 19:13:44 +0200

    Migrated code from Clients sample

---
+++ source/AccessTokenValidation/ReferenceTokens/ReferenceTokenValidationOptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin.Security;
+
+namespace Thinktecture.IdentityServer.v3.AccessTokenValidation
+{
+    public class ReferenceTokenValidationOptions : AuthenticationOptions
+    {
+        public ReferenceTokenValidationOptions() : base("IdSrvReferenceToken")
+        { }
+        
+        public string Authority { get; set; }
+    }
+}
