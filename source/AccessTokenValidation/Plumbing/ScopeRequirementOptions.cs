b4f73874436ed084b141ba467f4ecb80ec951fc3
Dominick Baier <dbaier@leastprivilege.com>
2016-01-04 13:15:17 +0100

    added comments

--- source/AccessTokenValidation/Plumbing/ScopeRequirementOptions.cs
+++ source/AccessTokenValidation/Plumbing/ScopeRequirementOptions.cs
@@ -2,9 +2,19 @@
 
 namespace IdentityServer3.AccessTokenValidation
 {
+    /// <summary>
+    /// Options for validating scope claims
+    /// </summary>
     public class ScopeRequirementOptions
     {
+        /// <summary>
+        /// Specifies which authentication type should be used (uses primary identity if empty)
+        /// </summary>
         public string AuthenticationType { get; set; }
+
+        /// <summary>
+        /// Specifies the accepted scope values
+        /// </summary>
         public IEnumerable<string> RequiredScopes { get; set; }
     }
 }
